251afa6c2f02f83b14a20848de1011907b87a94d
yildirimkadir <yildirimkad34@gmail.com>
2022-08-16 15:27:59 +0200

    " Cards updated"

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,38 @@
-import CardsStyle from "./Cards.style";
+import CardsStyle, {
+  Card,
+  Header,
+  MainContainer,
+  Image,
+  Button,
+} from "./Cards.style";
+import defaultImage from "../../assets/default-image.jpg";
+import { useNavigate } from "react-router-dom";
 
-const Cards = () => {
-  return <div>Cards</div>;
+const Cards = ({ info }) => {
+  const navigate = useNavigate();
+  return (
+    <MainContainer wrap="wrap">
+      {info?.map((item, index) => {
+        const {
+          recipe: { label, image },
+        } = item;
+        return (
+          <Card key={index}>
+            <Header>{label}</Header>
+            <Image src={image} />
+            <Button
+              onClick={() =>
+                navigate("/detail", { state: item, replace: false })
+              }
+            >
+              Show More
+            </Button>
+          </Card>
+        );
+      })}
+      ;
+    </MainContainer>
+  );
 };
 
 export default Cards;
