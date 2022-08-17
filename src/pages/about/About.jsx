2dff4cd70b78318526e70990b5e391763e81d83d
yildirimkadir <yildirimkad34@gmail.com>
2022-08-17 13:39:06 +0200

    "Das Projekt wurde erledigt."

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,33 @@
-import AboutStyle from "./About.style";
+import { AboutContainer, InfoContainer, StyledImage } from "./About.style";
+import codingSvg from "../../assets/coding.svg";
+// import { AiFillLinkedin } from "react-icons/ai";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <AboutContainer wrap="wrap">
+      <StyledImage src={codingSvg} />
+      <div>
+        <h1>About Software Developer</h1>
+      </div>
+      <InfoContainer>
+        <h2>Hi, I'am Kadir</h2>
+        <h3>I’m currently learning Full-Stack Development Languages.</h3>
+        <h4>
+          I've already known HTML, CSS, JS, ReactJS, Redux, Redux-Toolkit,
+          Redux-Thunk, SQLite, Python, Git & Github.
+        </h4>
+        <h3>
+          <a href="https://www.linkedin.com/in/kadir-yildirim-87b458230/">
+            Connect with me on Linkedin
+          </a>
+          <br />
+          <a href="https://github.com/yildirimkadir">
+            Follow me on Github
+          </a>{" "}
+        </h3>
+      </InfoContainer>
+    </AboutContainer>
+  );
 };
 
 export default About;
